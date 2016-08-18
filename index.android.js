28b2a41cda0b7cb43a3cccc3c57100868270709f
xyingyingme <xyingyingme@163.com>
2016-08-18 14:04:10 +0800

    add a page for aiyiqi site

--- index.android.js
+++ index.android.js
@@ -6,48 +6,43 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    ScrollView,
+    Dimensions,
 } from 'react-native';
+var Header = require('./app/header');
+var MovieTicket = require('./app/ticket');
+var ZhuanPan = require('./app/zhuanpan');
+var Reward = require('./app/reward');
+var Info = require('./app/info');
+var Footer = require('./app/footer');
+
+let ScreenWidth = Dimensions.get("window").width;
 
 class ReadingReactNative extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <ScrollView style={styles.container}>
+            <Header/>
+            <MovieTicket/>
+            <ZhuanPan/>
+            <Reward />
+            <Info />
+            <Footer/>
+        </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        // justifyContent: 'flex-start',
+        // alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
 });
+
 
 AppRegistry.registerComponent('ReadingReactNative', () => ReadingReactNative);
